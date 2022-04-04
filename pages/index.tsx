d9571a4b75ed293631708732cef0ed04eb02c0ad
YUNG WING WA <>
2022-04-04 13:33:13 +0800

    ✨feature: add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from "next";
+import Head from "next/head";
 import { CountryType } from "@type/countryType";
 import _ from "lodash";
 import { CountryTable } from "@components/table/CountryTable";
@@ -9,19 +10,24 @@ import useCountryFilterData from "@hook/useCountryFilterData";
 export default function Home({ data }: { data: CountryType[] }) {
     const { dataDisplayed, totalPage } = useCountryFilterData(data);
     return (
-        <div className="px-5">
-            <h1 className="my-2 my-md-3">GDP per person employed (in USD)</h1>
-            <div className="d-flex flex-column flex-md-row gap-3">
-                <div className="w-md-50 order-md-first order-last">
-                    <CountryTable countries={dataDisplayed} />
+        <>
+            <Head>
+                <title>GDP per person employed</title>
+            </Head>
+            <div className="px-5">
+                <h1 className="my-2 my-md-3">GDP per person employed (in USD)</h1>
+                <div className="d-flex flex-column flex-md-row gap-3">
+                    <div className="w-md-50 order-md-first order-last">
+                        <CountryTable countries={dataDisplayed} />
+                    </div>
+                    <div className="w-md-50">
+                        <Form />
+                    </div>
                 </div>
-                <div className="w-md-50">
-                    <Form />
-                </div>
+                <div className="mt-3"></div>
+                <Pages totalPage={totalPage} />
             </div>
-            <div className="mt-3"></div>
-            <Pages totalPage={totalPage} />
-        </div>
+        </>
     );
 }
 
@@ -32,8 +38,8 @@ export const getStaticProps: GetStaticProps = async () => {
         props: {
             data: _.map(
                 _.filter(_.slice(resultArray?.[1], 49), (o: CountryType) => o?.value), // get only the countries that have GNP data
-                (o: CountryType) => ({ ...o, value: Math.trunc(o.value) }) // truncate GDP value to integer 
-            ), 
+                (o: CountryType) => ({ ...o, value: Math.trunc(o.value) }) // truncate GDP value to integer
+            ),
         },
     };
 };
