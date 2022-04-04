a3baf05541a5525a06695e8bd9bf70c89c2f5726
YUNG WING WA <>
2022-04-04 11:29:14 +0800

    🚅perfs: refactor Form

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,56 +2,66 @@ import { FC, useEffect } from "react";
 import route, { useRouter } from "next/router";
 import styles from "@styles/form.module.scss";
 import { FormEvent, useState } from "react";
-const GREATER = "GREATER";
-const LESS = "LESS";
-export const Form:FC = () => {
+
+enum Input {
+    Country = "Country",
+    Greater = "Greater",
+    Less = "Less",
+}
+
+export const Form: FC = () => {
     const router = useRouter();
     const [country, setCountry] = useState<string>("");
     const [greaterThan, setGreaterThan] = useState<number | "">("");
     const [lessThan, setLessThan] = useState<number | "">("");
 
-    const gnpInputOnChange = (e: FormEvent<HTMLInputElement>, type: typeof GREATER | typeof LESS) => {
+    const input = (id: string, placeholder: string, value: string | number, onChange: (e: FormEvent<HTMLInputElement>) => void) => (
+        <input type={id === Input.Country ? "text" : "number"} className="px-3 py-2 rounded-3 w-100 w-md-auto" id={id} placeholder={placeholder} value={value} onChange={onChange} />
+    );
+    const inputOnChange = (e: FormEvent<HTMLInputElement>, type: Input) => {
         const target = e.target as HTMLInputElement;
-        if (type === GREATER) {
+        if (type === Input.Country) {
+            setCountry(target.value);
+        } else if (type === Input.Greater) {
             setGreaterThan(parseInt(target.value));
         } else {
             setLessThan(parseInt(target.value));
         }
     };
+    const searchResult = { ...(country && { country }), ...(greaterThan && { greaterThan }), ...(lessThan && { lessThan }) };
+
     useEffect(() => {
         setCountry((route.query.country as string) || "");
         setGreaterThan(Number(route.query.greaterThan) || "");
         setLessThan(Number(route.query.lessThan) || "");
     }, [router]);
-    const result = { ...(country && { country }), ...(greaterThan && { greaterThan }), ...(lessThan && { lessThan }) };
-    const input = (id: string, placeholder: string, value: string | number, onChange: (e: FormEvent<HTMLInputElement>) => void) => <input type={id === "country" ? "text" : "number"} className="px-3 py-2 rounded-3 w-100 w-md-auto" id={id} placeholder={placeholder} value={value} onChange={onChange} />;
+
     return (
         <>
             <div className="mb-3">
-                <label htmlFor="country" className="form-label d-block mb-2">
+                <label htmlFor={Input.Country} className="form-label d-block mb-2">
                     Country Name
                 </label>
-                {input("country", "Country", country, (e: FormEvent<HTMLInputElement>) => {
-                    const target = e.target as HTMLInputElement;
-                    setCountry(target.value);
+                {input(Input.Country, "Country", country, (e: FormEvent<HTMLInputElement>) => {
+                    inputOnChange(e, Input.Country);
                 })}
             </div>
             <div className="mb-3">
-                <label htmlFor="greaterThan" className="form-label d-block me-3 mb-2">
+                <label htmlFor={Input.Greater} className="form-label d-block me-3 mb-2">
                     GNP per Capital
                 </label>
                 <div className="d-flex">
-                    {input("greaterThan", "Greater than", greaterThan, (e: FormEvent<HTMLInputElement>) => {
-                        gnpInputOnChange(e, GREATER);
+                    {input(Input.Greater, "Greater than", greaterThan, (e: FormEvent<HTMLInputElement>) => {
+                        inputOnChange(e, Input.Greater);
                     })}
                     <span className="mx-3">-</span>
-                    {input("lessThan", "Smaller than", lessThan, (e: FormEvent<HTMLInputElement>) => {
-                        gnpInputOnChange(e, LESS);
+                    {input(Input.Less, "Smaller than", lessThan, (e: FormEvent<HTMLInputElement>) => {
+                        inputOnChange(e, Input.Less);
                     })}
                 </div>
             </div>
             <div className="d-flex justify-content-between justify-content-md-start">
-                <button className={`btn btn-primary ${styles.button}`} onClick={() => router.push({ pathname: router.pathname, query: { ...router.query, ...result, page: 1 } }, undefined, { shallow: true })}>
+                <button className={`btn btn-primary ${styles.button}`} onClick={() => router.push({ pathname: router.pathname, query: { ...router.query, ...searchResult, page: 1 } }, undefined, { shallow: true })}>
                     Search
                 </button>
                 <div className="ms-3"></div>
