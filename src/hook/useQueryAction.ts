1341a8d249a96331b10c6f8b52e0c74dabbdc918
YUNG WING WA <>
2022-06-12 23:35:43 +0800

    🚅perfs: move queryReducer outside to prevent instance recreation between re-renders

--- src/hook/useQueryAction.ts
+++ src/hook/useQueryAction.ts
@@ -19,38 +19,39 @@ type ACTIONTYPE = {
     } & Partial<FormType>;
 };
 
+const queryReducer = (state: ParsedUrlQueryInput, action: ACTIONTYPE): ParsedUrlQueryInput => {
+    const { type, payload: { orderAsc, orderBy, page } = {} } = action;
+
+    switch (type) {
+        case ACTIONS_QUERY.CHANGE_PAGE:
+            return { ...state, page };
+        case ACTIONS_QUERY.RESET:
+            return {};
+        case ACTIONS_QUERY.SORT:
+            return { ...state, orderBy, orderAsc, page: 1 };
+        case ACTIONS_QUERY.SUBMIT:
+            const dataToURL = Object.fromEntries(Object.entries(action.payload).filter(([_, v]) => v != ""));
+            return { ...state, ...dataToURL, page: 1 };
+        default:
+            return state;
+    }
+};
+
 const useQueryAction = () => {
     const router = useRouter();
     const { pathname, query } = router;
 
-    const queryReducer = (action: ACTIONTYPE): ParsedUrlQueryInput => {
-        const { type, payload: { orderAsc, orderBy, page } = {} } = action;
-
-        switch (type) {
-            case ACTIONS_QUERY.CHANGE_PAGE:
-                return { ...query, page };
-            case ACTIONS_QUERY.RESET:
-                return {};
-            case ACTIONS_QUERY.SORT:
-                return { ...query, orderBy, orderAsc, page: 1 };
-            case ACTIONS_QUERY.SUBMIT:
-                const dataToURL = Object.fromEntries(Object.entries(action.payload).filter(([_, v]) => v != ""));
-                return { ...query, ...dataToURL, page: 1 };
-            default:
-                return query;
-        }
-    };
-
-    const dispatchQuery = (action: ACTIONTYPE) => router.push({ pathname, query: queryReducer(action) }, undefined, { shallow: true });
+    const dispatchQuery = (action: ACTIONTYPE) => router.push({ pathname, query: queryReducer(query, action) }, undefined, { shallow: true });
 
     const sortLinkProp = (orderBy: `${OrderByEnum}`) => ({
         href: {
             pathname,
-            query: queryReducer({ type: ACTIONS_QUERY.SORT, payload: { orderBy, orderAsc: query.orderAsc !== "true" } }),
+            query: queryReducer(query, { type: ACTIONS_QUERY.SORT, payload: { orderBy, orderAsc: query.orderAsc !== "true" } }),
         },
         shallow: true,
     });
 
     return { ACTIONS_QUERY, dispatchQuery, sortLinkProp };
 };
+
 export default useQueryAction;
